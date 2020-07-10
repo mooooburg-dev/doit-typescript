d8ce36f50fdb51638655d839f9361b58dad4d3d5
mooooburg-dev <jeongmupark@gmail.com>
2020-07-10 16:50:19 +0900

    모듈화

--- ch02-2/src/index.ts
+++ ch02-2/src/index.ts
@@ -1,26 +1,9 @@
-let MAX_AGE = 100
-
-interface IPserson {
-  name: string
-  age: number
-}
-
-class Person implements IPserson {
-  constructor(public name: string, public age: number) {
-    
-  }
-}
-
-function makeRandomNumber(max: number = MAX_AGE): number {
-  return Math.ceil((Math.random() * max))
-}
-
-const makePerson = (name: string, age: number = makeRandomNumber()) => ({ name, age })
+import { IPerson, makePerson } from "./person/Person";
 
 const testMakePerson = (): void => {
-  let jane: IPserson = makePerson('Jane')
-  let jack: IPserson = makePerson('Jack')
-  console.log(jane, jack)
-}
+  let jane: IPerson = makePerson("Jane");
+  let jack: IPerson = makePerson("Jack");
+  console.log(jane, jack);
+};
 
-testMakePerson()
+testMakePerson();
